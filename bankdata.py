31cabeb8db2e453aabe4cffd60611007bb4395aa
Ghanmidev2012 <242857573+Ghanmidev2012@users.noreply.github.com>
2026-03-08 11:43:32 +0000

    Add files via upload

---
+++ bankdata.py
@@ -0,0 +1,8 @@
+import time
+
+print("your money in bank is 0$")
+
+money=input(("write e to enter your money:"))
+if input()=="e":
+    print(input("input your money here:"))
+print("your money is ",money,"$")
